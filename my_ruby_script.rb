626247e753429c478e48bf9decd45d88f3ed39b9
training <training@RubyRailsVM-16.courses.ebi.ac.uk>
2017-10-23 13:09:23 +0100

    Store block in a variable and use it

--- my_ruby_script.rb
+++ my_ruby_script.rb
@@ -1,3 +1,21 @@
-require_relative 'hello_world'
+######################################
+# Require a class
 
+require_relative 'hello_world'
 HelloWorld.new('Yuci').say_hi()
+
+######################################
+# Store block in a variable and use it
+# https://stackoverflow.com/questions/10757991/save-and-re-use-block-for-method-calls
+
+foo = lambda do |a|
+  puts a
+end
+
+[1, 2, 3, 4].each &foo
+
+boo = proc do |a|
+  puts "value is #{a}"
+end
+
+[1, 2, 3, 4].each &boo
