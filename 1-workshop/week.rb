0261cdebe7985bbb3494167c74a39f8bbd841fbc
training <training@RubyRailsVM-16.courses.ebi.ac.uk>
2017-10-23 13:55:28 +0100

    Refactoring

--- 1-workshop/week.rb
+++ 1-workshop/week.rb
@@ -5,13 +5,15 @@ class Week
   def initialize(number)
     @number = number
 
-    (1..7).each do |day|
-      if (@days)
-        @days.push Day.new(day)
-      else
-        @days = Array.new [Day.new(day)]
-      end
+    @days = (1..7).map do |day|
+      Day.new(day)
     end
+=begin
+    (1..7).each do |day|
+      @days ||= Array.new()
+      @days.push Day.new(day)
+    end
+=end
     # @days = [Day.new(0), Day.new(1), Day.new(2), Day.new(3), Day.new(4), Day.new(5), Day.new(6)]
   end
 end
--- 1-workshop/year.rb
+++ 1-workshop/year.rb
@@ -4,6 +4,11 @@ class Year
   def initialize(number)
     @number = number
 
+    @weeks = (1..52).map do |week|
+      Week.new(week)
+    end
+
+=begin
     (1..52).each do |week|
       if (@weeks)
         @weeks.push Week.new(week)
@@ -11,5 +16,7 @@ class Year
         @weeks = Array.new [Week.new(week)]
       end
     end
+=end
+
   end
 end
